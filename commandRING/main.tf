908c7f5b963c5caffdfc082dba35c54151e50223
Jacek Kuśnierz <kusnierz@protonmail.com>
2020-03-23 10:49:45 +0100

    test 100 workers (wow)

--- commandRING/main.tf
+++ commandRING/main.tf
@@ -3,7 +3,7 @@ provider "docker" {
 }
 
 resource "docker_container" "worker" {
-  count = 10
+  count = 100
   name  = "${format("sample_worker-%d", count.index)}"
   image = "worker_image:latest"
   # command = ""
