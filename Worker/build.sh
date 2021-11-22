eaee2be00e33d117019781669cbc81fd6d5950f6
Kamilbur <kamil.burkiewicz@gmail.com>
2021-11-22 20:57:24 +0100

    Automatic update of AWS Backend

--- Worker/build.sh
+++ Worker/build.sh
@@ -1,3 +1,15 @@
 #!/bin/bash -ex
 
+ROOT_BRANCH_NAME=${1:-AWS-dev}
+
+if [[ ! -d ./root ]] ; then
+    git clone -b ${ROOT_BRANCH_NAME} https://github.com/CloudPyRDF/root.git
+else
+    cd root
+    git pull
+    git checkout ${ROOT_BRANCH_NAME}
+    cd ..
+fi
+
+
 sudo DOCKER_BUILDKIT=1 docker build . -t root_worker:latest
